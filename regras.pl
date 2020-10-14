798b06593414a6bf9ebd6692dfbc1df1ce05b803
Eduardo Lima <eduardolimrib@gmail.com>
2020-10-14 18:01:25 -0300

    Update rules

--- regras.pl
+++ regras.pl
@@ -1,11 +1,15 @@
 :- [base_dados].
 
 principal_suspeito(Pessoa, Crime) :-
-  crime(Crime,Vitima,Dia,Lugar),
+  crime(Crime,Vitima,Dia,Lugar, Objeto),
   possivel_suspeito(Pessoa),
+  frequenta(Pessoa, Lugar),
   estava(Pessoa,Dia,Lugar),
   tem_motivo_contra(Pessoa,Vitima).
 principal_suspeito(desconhecido,_).
 
 tem_motivo_contra(Pessoa,Vitima):-
   inveja(Pessoa,Vitima).
+
+objetos_no_lugar(Lugar) :-
+  tem_objeto(Lugar, Objeto).
